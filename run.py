6ae211bf88c502fa77093cc11ad18f420bc14a9a
IllyaPetrusenko <illya.petrusenko@gmail.com>
2023-03-16 11:44:05 +0200

    add ssl

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
-from flask_sslify import SSLify
+# from flask_sslify import SSLify
 
 app = Flask(__name__, template_folder='application/templates', static_folder='application/static')
-sslify = SSLify(app)
+# sslify = SSLify(app)
 
 
 @app.route("/")
