fe88f41d2d8c6f685725442a8c4eeadc0906f096
IllyaPetrusenko <illya.petrusenko@gmail.com>
2023-03-16 18:22:59 +0200

    add cert and key to Dockerfile

--- run.py
+++ run.py
@@ -9,4 +9,4 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, ssl_context=('fullchain1.pem', 'privkey1.pem'))
+    app.run(host='0.0.0.0', ssl_context=('fullchain1.pem', 'privkey1.pem'))
